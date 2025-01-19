2f82eb89f7190aeac530f4da5bc4744201edff52
Alex Finley <alexfin@outlook.com>
2025-01-19 15:16:47 -0500

    clean up

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:castle_walls/features/login/login.dart';
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -26,24 +25,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-  var favorites = <WordPair>[];
-
-  void newIdea() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
-    } else {
-      favorites.add(current);
-    }
-    notifyListeners();
-  }
-}
+class MyAppState extends ChangeNotifier {}
 
 // ...
 
@@ -69,23 +51,6 @@ class _MyHomePageState extends State<MyHomePage> {
       return Scaffold(
         body: Row(
           children: [
-            // SafeArea(
-            //   child: NavigationRail(
-            //     extended: constraints.maxWidth >= 600,
-            //     destinations: [
-            //       NavigationRailDestination(
-            //         icon: Icon(Icons.login),
-            //         label: Text('Login'),
-            //       ),
-            //     ],
-            //     selectedIndex: selectedIndex,
-            //     onDestinationSelected: (value) {
-            //       setState(() {
-            //         selectedIndex = value;
-            //       });
-            //     },
-            //   ),
-            // ),
             Expanded(
               child: Container(
                 color: Theme.of(context).colorScheme.primaryContainer,
@@ -96,119 +61,5 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       );
     });
-  }
-}
-
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.newIdea();
-                },
-                child: Text('New Idea'),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class FavoritesPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var favorites = appState.favorites;
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text(
-            'Favorites',
-            style: Theme.of(context).textTheme.displayMedium!.copyWith(
-                  color: Theme.of(context).colorScheme.onPrimaryContainer,
-                ),
-          ),
-        ),
-        ListView.builder(
-          itemCount: favorites.length,
-          itemBuilder: (context, index) {
-            return BigCard(pair: favorites[index]);
-          },
-          shrinkWrap: true,
-        ),
-      ],
-    );
-  }
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onTertiary,
-    );
-
-    return Card(
-      color: theme.colorScheme.tertiary,
-      child: AnimatedSize(
-        duration: Duration(milliseconds: 200),
-        // Make sure that the compound word wraps correctly when the window
-        // is too narrow.
-        child: MergeSemantics(
-          child: Wrap(
-            children: [
-              Text(
-                pair.first,
-                style: style.copyWith(fontWeight: FontWeight.w200),
-              ),
-              Text(
-                pair.second,
-                style: style.copyWith(fontWeight: FontWeight.bold),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
   }
 }
